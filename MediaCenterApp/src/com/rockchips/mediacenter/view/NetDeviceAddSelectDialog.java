13a1a7245fa0bb3592734de12a1d7eea0f9fbd84
GaoFei <fly.gao@rock-chips.com>
2017-05-12 15:48:53 +0800

    Hide start home media share for 7.1.
    Signed-off-by: GaoFei <fly.gao@rock-chips.com>

--- MediaCenterApp/src/com/rockchips/mediacenter/view/NetDeviceAddSelectDialog.java
+++ MediaCenterApp/src/com/rockchips/mediacenter/view/NetDeviceAddSelectDialog.java
@@ -1,4 +1,8 @@
 package com.rockchips.mediacenter.view;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import momo.cn.edu.fjnu.androidutils.utils.NetWorkUtils;
 import momo.cn.edu.fjnu.androidutils.utils.ResourceUtils;
 import momo.cn.edu.fjnu.androidutils.utils.ToastUtils;
@@ -8,10 +12,12 @@ import org.xutils.view.annotation.ViewInject;
 import android.content.Context;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.rockchips.mediacenter.R;
 import com.rockchips.mediacenter.data.ConstData;
+import com.rockchips.mediacenter.utils.PlatformUtils;
 /**
  * @author GaoFei
  * 网络设备添加选择对话框
@@ -50,6 +56,16 @@ public class NetDeviceAddSelectDialog extends AppBaseDialog {
 
 	@Override
 	public void initData() {
+		if(PlatformUtils.getSDKVersion() <= 19){
+			String[] oprations = mContext.getResources().getStringArray(R.array.net_devices);
+			List<String> oprationList = new ArrayList<>();
+			for(String itemOpration : oprations){
+				oprationList.add(itemOpration);
+			}
+			oprationList.add(mContext.getString(R.string.start_home_media_share));
+			ArrayAdapter<String> adapter = new ArrayAdapter<>(mContext, android.R.layout.simple_list_item_1, android.R.id.text1, oprationList);
+			mListNetAddSelect.setAdapter(adapter);
+		}
 		
 	}
 
